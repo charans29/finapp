d5b9c9ffa2462e38884a09cc01eb7eed79ebe928
charans29 <smodugula@csuchico.edu>
2025-06-22 17:08:33 -0700

    Add GitHub Actions CI/CD pipeline

--- src/test/java/com/springboot/finapp/FinappApplicationTests.java
+++ src/test/java/com/springboot/finapp/FinappApplicationTests.java
@@ -1,13 +1,24 @@
 package com.springboot.finapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class FinappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	public void HelloController() throws Exception {
+		mockMvc.perform(get("/hello"))
+				.andExpect(status().isOk());
+	}
 }
