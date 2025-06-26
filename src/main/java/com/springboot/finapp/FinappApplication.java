6b20613b130b29c1edb457778956824191c5859b
charans29 <smodugula@csuchico.edu>
2025-06-26 13:43:18 -0700

    ci fix done

--- src/main/java/com/springboot/finapp/FinappApplication.java
+++ src/main/java/com/springboot/finapp/FinappApplication.java
@@ -1,22 +1,3 @@
-// package com.springboot.finapp;
-
-// import com.springboot.finapp.config.CassandraConfig; 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.Import;
-// import org.springframework.context.annotation.ComponentScan;
-
-// @SpringBootApplication
-// @ComponentScan(basePackages = "com.springboot.finapp")
-// @Import({CassandraConfig.class})
-// public class FinappApplication {
-
-//     public static void main(String[] args) {
-//         SpringApplication.run(FinappApplication.class, args);
-//     }
-// }
-
-
 package com.springboot.finapp;
 
 import org.springframework.boot.SpringApplication;
