433ab1b3cae54a14bc09b617c4b4ca72aa2d891f
charans29 <smodugula@csuchico.edu>
2025-06-26 13:31:39 -0700

    rm cassconfig

--- src/main/java/com/springboot/finapp/config/CassandraConfig.java
+++ src/main/java/com/springboot/finapp/config/CassandraConfig.java
@@ -1,40 +1,40 @@
-package com.springboot.finapp.config;
+// package com.springboot.finapp.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
-import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+// import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
-@Configuration
-@EnableCassandraRepositories(basePackages = "com.springboot.finapp.repository")
-public class CassandraConfig extends AbstractCassandraConfiguration {
+// @Configuration
+// @EnableCassandraRepositories(basePackages = "com.springboot.finapp.repository")
+// public class CassandraConfig extends AbstractCassandraConfiguration {
 
-    @Value("${spring.data.cassandra.keyspace-name}")
-    private String keyspaceName;
+//     @Value("${spring.data.cassandra.keyspace-name}")
+//     private String keyspaceName;
 
-    @Value("${spring.data.cassandra.contact-points:cassandra}")
-    private String contactPoints;
+//     @Value("${spring.data.cassandra.contact-points:cassandra}")
+//     private String contactPoints;
 
-    @Value("${spring.data.cassandra.port:9042}")
-    private int port;
+//     @Value("${spring.data.cassandra.port:9042}")
+//     private int port;
 
-    @Override
-    protected String getKeyspaceName() {
-        return keyspaceName;
-    }
+//     @Override
+//     protected String getKeyspaceName() {
+//         return keyspaceName;
+//     }
 
-    @Override
-    protected String getContactPoints() {
-        return contactPoints;
-    }
+//     @Override
+//     protected String getContactPoints() {
+//         return contactPoints;
+//     }
 
-    @Override
-    protected int getPort() {
-        return port;
-    }
+//     @Override
+//     protected int getPort() {
+//         return port;
+//     }
 
-    @Override
-    public String[] getEntityBasePackages() {
-        return new String[] { "com.springboot.finapp.model" };
-    }
-}
+//     @Override
+//     public String[] getEntityBasePackages() {
+//         return new String[] { "com.springboot.finapp.model" };
+//     }
+// }
